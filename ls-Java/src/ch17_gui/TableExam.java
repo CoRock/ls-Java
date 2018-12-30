7fa6be58dc47ab0de8c7ff2f91ff4a21c365baaa
CoRock <dycorphin@gmail.com>
2018-12-30 23:53:38 +0900

    Add JTable using WindowBuilder in eclipse

---
+++ ls-Java/src/ch17_gui/TableExam.java
@@ -0,0 +1,64 @@
+package ch17_gui;
+
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
+
+public class TableExam extends JFrame {
+
+	private JPanel contentPane;
+	private JTable table;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					TableExam frame = new TableExam();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public TableExam() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 300);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		contentPane.setLayout(new BorderLayout(0, 0));
+		setContentPane(contentPane);
+		
+		JScrollPane scrollPane = new JScrollPane();
+		contentPane.add(scrollPane, BorderLayout.CENTER);
+		
+		/**
+		 * 코드 추가
+		 * 
+		 * String[] title: 표의 제목열(title)
+		 * String[][] data: 표의 데이터(rows)		 * 
+		 * new JTable(rows, header)
+		 */
+		String[] title = { "번호", "이름", "입사일자" };
+		String[][] data = {
+				{ "1", "kim", "2017-01-01" },
+				{ "2", "park", "2017-02-01" },
+				{ "3", "hong", "2017-03-01" },				
+		};		
+		table = new JTable(data, title);
+		scrollPane.setViewportView(table);
+	}
+
+}
